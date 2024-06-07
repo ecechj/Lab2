269b5123d839c599d74a89d020575a79123f12d9
root <root@BOOK-PRAO6LLCM8>
2024-06-07 17:55:53 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -6,17 +6,27 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
-
-
-
-
+void func(int cnt, int start) {
+	if (cnt == M){
+		for (int i = 0; i < M;i++){
+			cout << vec[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	
+	for (int i = start; i <= N; i++){
+		vec.push_back(i);
+		func(cnt + 1, i + 1);
+		vec.pop_back();
+	}
 
 
 
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+	cin >> N >> M;
+	func(0,1);
+	return 0;
 }
